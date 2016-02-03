07e9f9053f16337aea4ed290716189787525f65b
Peter Marreck <lumbergh@gmail.com>
2016-02-03 13:32:23 -0500

    SUCCESS! Converted the maze generator to not use a mutable process dictionary AND be deterministic for testing

--- maze_generator.exs
+++ maze_generator.exs
@@ -1,32 +1,34 @@
 defmodule Maze do
-  def generate(w, h, rand_seed \\ :os.timestamp) do
+  def generate(w, h, maze \\ %{}, rand_seed \\ :os.timestamp) do
     :random.seed(rand_seed)
     :rand.seed(:exs64, rand_seed)
-    (for i <- 1..w, j <- 1..h, do: {i,j}) |>
-    Enum.each(fn{i,j} -> Process.put({:vis, i, j}, true) end)
-    walk(:random.uniform(w), :random.uniform(h))
-    get({w,h})
+    maze_tuples = (for i <- 1..w, j <- 1..h, do: {i,j})
+    maze = Enum.reduce(maze_tuples, maze, fn({i,j}, maze) -> Map.put(maze, {:vis, i, j}, true) end)
+    get(w, h, walk(:random.uniform(w), :random.uniform(h), maze))
   end
 
-  def print(w, h, rand_seed \\ :os.timestamp) do
-    Maze.generate(w, h, rand_seed) |> Enum.each(&IO.puts/1)
+  def print(w, h, maze \\ %{}, rand_seed \\ :os.timestamp) do
+    Maze.generate(w, h, maze, rand_seed) |> Enum.each(&IO.puts(&1))
   end
 
-  defp walk(x, y) do
-    Process.put({:vis, x, y}, false)
-    Enum.each(Enum.shuffle([[x-1,y], [x,y+1], [x+1,y], [x,y-1]]), fn [i,j] ->
-      if Process.get({:vis, i, j}) do
-        if i == x, do: Process.put({:hor, x, max(y, j)}, "+   "),
-                 else: Process.put({:ver, max(x, i), y}, "    ")
-        walk(i, j)
+  defp walk(x, y, %{} = maze) do
+    Enum.reduce(
+      Enum.shuffle([{x-1,y}, {x,y+1}, {x+1,y}, {x,y-1}]),
+      %{maze | {:vis, x, y} => false},
+      fn({i,j}, maze) ->
+        if maze[{:vis, i, j}] do
+          walk(i, j, (if i == x, do: Map.put(maze, {:hor, x, max(y, j)}, "+   "), else: Map.put(maze, {:ver, max(x, i), y}, "    ")))
+        else
+          maze
+        end
       end
-    end)
+    )
   end
 
-  defp get({w,h}) do
+  defp get(w, h, %{} = maze) do
     Enum.map(1..h, fn j ->
-      [(Enum.map(1..w, fn i -> Process.get({:hor, i, j}, "+---") end) |> Enum.join) <> "+",
-      (Enum.map(1..w, fn i -> Process.get({:ver, i, j}, "|   ") end) |> Enum.join) <> "|"]
+      [(Enum.map(1..w, fn i -> Map.get(maze, {:hor, i, j}, "+---") end) |> Enum.join) <> "+",
+       (Enum.map(1..w, fn i -> Map.get(maze, {:ver, i, j}, "|   ") end) |> Enum.join) <> "|"]
     end) ++ [String.duplicate("+---", w) <> "+"]
     |> List.flatten
   end
@@ -38,9 +40,10 @@ if System.argv |> List.first == "test" do
 
   defmodule MazeTest do
     use ExUnit.Case, async: true
+    @rand_seed {1454, 367294, 550155}
 
     test "5x5 output as expected" do
-      assert Maze.generate(5, 5, {1454, 367294, 550155}) == [
+      assert Maze.generate(5, 5, %{}, @rand_seed) == [
         "+---+---+---+---+---+",
         "|                   |",
         "+   +---+---+---+   +",
