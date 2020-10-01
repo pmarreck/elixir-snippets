b1af9d81c080111e1027dca3351e6a7221d782d3
Peter Marreck <peter@marreck.com>
2020-10-01 19:29:11 +0000

    added a test for the y combinator implementation

--- y_combinator.exs
+++ y_combinator.exs
@@ -8,4 +8,24 @@ defmodule Combinator do
   end
 end
 
-# TODO: write a test for it
+# run this inline suite with "elixir #{__ENV__.file} test"
+if System.argv |> List.first == "test" do
+  ExUnit.start
+
+  defmodule CombinatorTest do
+    use ExUnit.Case, async: true
+    alias Combinator, as: C
+
+    test "calling anonymous function recursively that does factorial" do
+      fact = C.fix(fn
+                     this_function ->
+                     fn
+                       0 -> 1
+                       x when x > 0 -> x * this_function.(x-1)
+                     end
+                   end)
+      assert 120 == fact.(5)
+    end
+
+  end
+end
