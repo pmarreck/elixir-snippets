2ebafc2f3e3317ca8ef2c96946ff0fd8fbf4b463
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-27 20:06:29 +0800

    Delete cool function to pull out a cookie value from an array of tuples.exs

--- cool function to pull out a cookie value from an array of tuples.exs
+++
@@ -1,5 +0,0 @@
- defmodule Cookie do
-   def run([ { "Set-Cookie", _} | [{"Set-Cookie", val} | _] ]), do: val
-   def run([ { "Set-Cookie", _} | _ ]), do: return
-   def run([ _ | rest ]), do: run(rest)
- end
